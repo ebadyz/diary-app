cbfe53f94a7d4886238e8686218cd66d9fb1d67f
ebadyz <ebad.yousefzade@gmail.com>
2021-05-24 21:38:14 +0430

    separated theme from App.js

--- App.js
+++ App.js
@@ -1,19 +1,7 @@
-import * as React from 'react';
-import {
-  Text,
-  View,
-} from "react-native";
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
-
-const theme = {
-  ...DefaultTheme,
-  roundness: 2,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: '#3498db',
-    accent: '#f1c40f',
-  },
-};
+import * as React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { Provider as PaperProvider } from "react-native-paper";
+import theme from './src/theme'
 
 const App = () => {
   return (
