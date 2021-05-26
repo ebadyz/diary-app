e2f14c9fe9b130c6ac166c29dd9598f454f668c3
ebadyz <ebad.yousefzade@gmail.com>
2021-05-26 14:53:52 +0430

    add screen created

---
+++ src/screens/add/index.js
@@ -0,0 +1,106 @@
+import * as React from "react";
+import { useContext } from "react";
+import { View, StyleSheet } from "react-native";
+import { TextInput, Button, Headline, Subheading } from "react-native-paper";
+import { useFormik } from "formik";
+import theme from "../../theme";
+import { AuthContext } from "../../contexts/auth";
+
+const fields = [
+  {
+    placeholder: "Title",
+    name: "title",
+    onChangeText: handleChange()
+  },
+  {
+    placeholder: "Diary",
+    name: "diary",
+  },
+];
+
+const addDiary = () => {
+  const {
+    values,
+    errors,
+    touched,
+    handleChange,
+    submitForm,
+    handleBlur,
+    isValid,
+    setFieldValue,
+    setFieldTouched,
+  } = useFormik({
+    initialValues: {
+      title: "",
+      diary: "",
+    },
+    // validationSchema: loginSchema,
+    onSubmit: async (values) => {
+    //   signIn();
+    },
+    enableReinitialize: true,
+    validateOnChange: true,
+    validateOnMount: true,
+  });
+
+  const renderedFields = fields.map((element, count) => {
+    const { label, name, onChangeText, onBlur, value } = element;
+    return (
+      <View key={"field-" + count} style={styles.spacing}>
+        <TextInput
+          mode="outlined"
+          label={label}
+          name={name}
+          onChangeText={onChangeText}
+          onBlur={onBlur}
+          value={value}
+          fullWidth
+        />
+      </View>
+    );
+  });
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.main}>{renderedFields}</View>
+      <View style={styles.footer}>
+        <Button
+          mode="contained"
+          uppercase={false}
+          color={theme.colors.primary}
+          style={styles.btn}
+          labelStyle={{ color: theme.colors.text }}
+          onPress={submitForm}
+        >
+          Create
+        </Button>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: theme.colors.background,
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 20,
+  },
+  spacing: {
+    paddingBottom: 10,
+  },
+  main: {
+    flex: 2,
+  },
+  btn: {
+    borderRadius: 5,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  footer: {
+    flex: 1,
+  },
+});
+
+export default Login;
