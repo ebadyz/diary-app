e9ff48595827a749d3e29c11baddab32fd0fadfd
ebadyz <ebad.yousefzade@gmail.com>
2021-05-27 16:26:02 +0430

    updated profile screen

--- src/screens/profile/index.js
+++ src/screens/profile/index.js
@@ -1,12 +1,76 @@
-import React from 'react'
-import { View } from 'react-native'
-import { Text } from 'react-native-paper'
-
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import {
+  Avatar,
+  Text,
+  Card,
+  Title,
+  Subheading,
+  Headline,
+} from "react-native-paper";
+import theme from "../../theme";
 
 export default function Profile() {
-    return(
+  return (
+    <View style={styles.container}>
+      <View style={styles.avatar}>
         <View>
-            <Text>Profile</Text>
+          <Avatar.Image
+            size={107}
+            source={{
+              uri: "https://qph.fs.quoracdn.net/main-qimg-2b21b9dd05c757fe30231fac65b504dd",
+            }}
+          />
         </View>
-    )
+        <Headline>ebad_yz</Headline>
+      </View>
+      <View style={styles.userInfo}>
+        <View style={styles.center}>
+          <Title style={styles.count}>12</Title>
+          <Subheading>Total Diaries</Subheading>
+        </View>
+        <View style={styles.center}>
+          <Title style={styles.count}>7</Title>
+          <Subheading>Fav Diaries</Subheading>
+        </View>
+      </View>
+      <View style={styles.items}>
+        <Card>
+          <Card.Title
+            title="Joined Date"
+            subtitle="1 january 2020"
+            left={(props) => <Avatar.Icon {...props} icon="folder" />}
+          />
+        </Card>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.colors.background,
+    paddingHorizontal: 20,
+  },
+  avatar: {
+    flex: 2,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  userInfo: {
+    flex: 2,
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  center: {
+    alignItems: "center",
+  },
+  count: {
+    color: theme.colors.primary,
+  },
+  items: {
+    flex: 2,
+  },
+});
