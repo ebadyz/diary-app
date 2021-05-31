79a154e7d772b805881f0d193ae5a135ed73c9db
Farzad Yousefzadeh <farskid@gmail.com>
2021-05-31 16:26:32 +0300

    sort diaries by date desc

--- src/queries/allDiaries.js
+++ src/queries/allDiaries.js
@@ -1,11 +1,15 @@
 export function allDiariesQuery(db, onSuccess) {
   return db.transaction((tx) => {
-    tx.executeSql("SELECT * FROM Diaries", [], (_, results) => {
-      onSuccess(
-        Array.from({ length: results.rows.length })
-          .fill()
-          .map((_, i) => results.rows.item(i))
-      );
-    });
+    tx.executeSql(
+      "SELECT * FROM Diaries ORDER BY Date DESC",
+      [],
+      (_, results) => {
+        onSuccess(
+          Array.from({ length: results.rows.length })
+            .fill()
+            .map((_, i) => results.rows.item(i))
+        );
+      }
+    );
   });
 }
