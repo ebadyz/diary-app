4408068a0fa59db3120d3d08e8b73778651efb23
ebadyz <ebad.yousefzade@gmail.com>
2021-05-31 23:18:17 +0430

    change styles of detail screen

--- src/screens/detail/index.js
+++ src/screens/detail/index.js
@@ -1,15 +1,49 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import { View, StyleSheet, ActivityIndicator, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
+import {
+  Text,
+  ActivityIndicator,
+  Paragraph,
+  Title,
+  Subheading,
+} from "react-native-paper";
 import { useDB } from "../../hooks/useDB";
 import { diaryDetailQuery } from "../../queries/diaryDetail";
+import { useTheme } from "@react-navigation/native";
+import { ScrollView } from "react-native-gesture-handler";
 
 const DiaryDetail = ({ route }) => {
   const { itemId } = route.params;
   console.log("detail page", { itemId });
   const db = useDB();
+  const { colors } = useTheme();
 
   const [itemState, setItemState] = useState({ state: "pending", item: null });
+
+  const styles = StyleSheet.create({
+    container: {
+      backgroundColor: colors.surface,
+      flex: 1,
+      marginVertical: 15,
+      marginHorizontal: 10,
+      paddingVertical: 40,
+      paddingHorizontal: 30,
+      borderRadius: 50,
+    },
+    indicator: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    title: {
+      color: colors.text,
+      marginBottom: 15,
+    },
+    diary: {
+      color: colors.text,
+    },
+  });
 
   useEffect(() => {
     diaryDetailQuery(db, { id: itemId }, (item) => {
@@ -23,45 +57,22 @@ const DiaryDetail = ({ route }) => {
 
   if (itemState.state === "pending") {
     return (
-      <View style={styles.container}>
-        <ActivityIndicator color="blue" size="large" />
+      <View style={styles.indicator}>
+        <ActivityIndicator
+          color={colors.primary}
+          size="large"
+          animating={true}
+        />
       </View>
     );
   }
 
   return (
-    <>
-      <View style={styles.container}>
-        <Text style={{ color: "black" }}>title: {itemState.item.Title}</Text>
-        <Text style={{ color: "black" }}>diary: {itemState.item.Diary}</Text>
-      </View>
-    </>
+    <ScrollView style={styles.container}>
+      <Subheading style={styles.title}>{itemState.item.Title}</Subheading>
+      <Paragraph style={styles.diary}>{itemState.item.Diary}</Paragraph>
+    </ScrollView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor:"#ffffff",
-    flex: 1,
-    justifyContent: "center",
-    paddingHorizontal: 20,
-    paddingVertical: 20,
-  },
-  spacing: {
-    paddingBottom: 10,
-  },
-  main: {
-    flex: 6,
-  },
-  btn: {
-    borderRadius: 5,
-    height: 50,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  footer: {
-    flex: 1,
-  },
-});
 
 export default DiaryDetail;
